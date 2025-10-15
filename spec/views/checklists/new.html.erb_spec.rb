9cd4de38edabbdcd3052988ba86b45f94085d874
gustvohnsv <guvi10vieira@gmail.com>
2025-10-15 13:11:38 -0300

    Adicionei o necessário para os testes RSpec das checklists
    - Modifiquei o Controller de 'Checklists' para ficar mais elegante
    - Adicionei um conjunto mínimo de testes para as views 'checklists/show' e 'checklists/new'
    - Adicionei um conjunto mínimo de testes para o Controller de 'Checklists'
    - E outras mudanças pequenas

--- spec/views/checklists/new.html.erb_spec.rb
+++ spec/views/checklists/new.html.erb_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "checklists/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let!(:user) {FactoryBot.create(:user)}
+  let!(:checklist) {FactoryBot.create(:checklist, user: user)}
+
+  before(:each) do
+    assign(:user, user)
+    assign(:checklist, checklist)
+    render
+  end
+
+  it 'deve conter um campo de texto para o título' do
+    expect(rendered).to have_field('Title')
+  end
+  it 'deve conter um botão para criar a checklist' do
+    expect(rendered).to have_button("Criar checklist")
+  end
 end
