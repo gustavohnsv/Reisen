617b7cd78355f0bbd3d16512b29befeb3ef6978c
Kaique de Jesus Pessoa Santos <kaique10defevereiro@gmail.com>
2025-10-15 22:13:15 -0300

    Alterei a interface para ficar mais agradavel quando preciso mexer no site, alterei a forma que os testes leem os links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :avatar, :remove_avatar])
   end
 end
