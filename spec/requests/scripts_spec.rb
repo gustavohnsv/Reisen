2f9930d87301fb03ca9cfe09eb66586bf9b5eb09
gustvohnsv <guvi10vieira@gmail.com>
2025-10-29 12:53:34 -0300

    Adicionei a base da funcionalidade para restringir permissões
    - Criei um módulo de suporte que lida com as permissões básicas do roteiro
    - Ajustei o controlador principal e o de item do roteiro para verificarem as permissões
    - Mudei as views para controlar o que o usuário pode ver dependendo da sua permissão
    - Mudei os testes unitários para que convidados via link só possam ver os itens

--- spec/requests/scripts_spec.rb
+++ spec/requests/scripts_spec.rb
@@ -66,7 +66,8 @@ RSpec.describe "Scripts", type: :request do
         other_script = FactoryBot.create(:script, user: other_user)
         FactoryBot.create(:participant, user: user, script: other_script)
         patch script_path(other_script.id), params: {script: {title: 'Roteiro atualizado'}}
-        expect(response).to redirect_to(root_path)
+        expect(response).to have_http_status(:unprocessable_content)
+        expect(response).to render_template("scripts/show")
       end
     end
   end
@@ -102,18 +103,6 @@ RSpec.describe "Scripts", type: :request do
       end
     end
   end
-  #describe 'GET /scripts/:id/edit' do -> 'edit' substituída para uma partial
-  #  context 'usuário está logado' do
-  #    before do
-  #      sign_in(user)
-  #    end
-  #    it 'acessa a página de edição do roteiro com sucesso' do
-  #      get edit_script_path(script.id)
-  #      expect(response).to have_http_status(:success)
-  #      expect(response).to render_template(:edit)
-  #    end
-  #  end
-  #end
   describe 'DELETE /scripts/:id' do
     context 'usuário está logado' do
       before do
