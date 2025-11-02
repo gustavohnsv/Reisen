e4e51f158ac25858c269c043ca4428ef20ab79ac
Renan Bezerra Guimarães <124211327+RenanBezerraGuima@users.noreply.github.com>
2025-11-02 12:04:18 -0300

    Set up Action Mailer SMTP configuration
    Configure Action Mailer for production with SMTP settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,24 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Use https:// se o seu site no Heroku tiver SSL (o que é o padrão)
+  config.action_mailer.default_url_options = {
+    protocol: 'https',
+    host: 'www.reisen-rails.tech'
+  }
+
+  # Informa ao Rails para não engolir erros de envio de e-mail em produção.
+  config.action_mailer.raise_delivery_method = :smtp
+
+  # Configurações para o servidor SMTP lidar com o e-mail
+  config.action_mailer.smtp_settings = {
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :domain               => ENV['SMTP_DOMAIN'],
+    :address              => ENV['SMTP_SERVER'],
+    :port                 => ENV['SMTP_PORT'],
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
 end
