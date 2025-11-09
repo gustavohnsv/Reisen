07fa10b00a3cd745a661fd2ee23d6424b916ce5d
Kaique de Jesus Pessoa Santos <kaique10defevereiro@gmail.com>
2025-11-09 19:37:24 -0300

    removi dev controller

--- app/controllers/dev_controller.rb
+++
@@ -1,4 +0,0 @@
-/* DevController removed. The dev auto-login route was removed from routes.rb. */
-class DevController
-  # intentionally empty - dev helper removed. Keep file to avoid failures in older branches.
-end
